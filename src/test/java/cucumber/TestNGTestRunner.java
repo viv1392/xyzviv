34f3f9cd27970318f452e6bb99aa2f6a9fffac47
Vivek Pandey <vivpandeyy@gmail.com>
2023-05-28 17:50:05 +0530

    SpaceX

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -6,5 +6,4 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/java/cucumber", glue = "purchaseOrder.stepDefenation", monochrome = true, plugin = {
 		"html:target/cucumber.html" }, tags="@smoke")
 public class TestNGTestRunner extends AbstractTestNGCucumberTests  {
-
 }
